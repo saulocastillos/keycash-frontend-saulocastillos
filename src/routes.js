a52a20913cb1f97eeecd811dd07d06a81f1b6f66
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-26 19:51:00 -0300

    remove hashrouter

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import Property from './pages/Property';
 
 export default function Routes() {
   return (
-      <HashRouter>
-        <Switch>
-          <Route exact path={'/'} component={Main} />
-          <Route path={'/property'} component={Property} />
-        </Switch>
-      </HashRouter>
+    <Switch>
+      <Route exact path="/" component={Main} />
+      <Route path="/property" component={Property} />
+    </Switch>
   );
 }
