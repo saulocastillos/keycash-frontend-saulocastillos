08aefaa270e1da858dd0433b73ec16d4e999de95
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-26 17:42:37 -0300

    routes added

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { HashRouter, Switch, Route } from 'react-router-dom'; 
+
+import Main from './pages/Main';
+import Property from './pages/Property';
+
+export default function Routes() {
+  return (
+      <HashRouter>
+        <Switch>
+          <Route exact path={'/'} component={Main} />
+          <Route path={'/property'} component={Property} />
+        </Switch>
+      </HashRouter>
+  );
+}
