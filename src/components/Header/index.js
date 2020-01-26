7afed7d2655bb719924d2531f49c98946a4ac86e
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-26 20:17:55 -0300

    add header component and styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { MdFavorite } from 'react-icons/md';
+
+import { Container, Favorite } from './styles';
+
+import logo from '../../assets/images/logo.svg';
 
 export default function Header() {
-  return <div />;
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Keycash Properties" />
+      </Link>
+      <Favorite>
+        <div>
+          <strong>Favoritos</strong>
+          <span>3 itens</span>
+        </div>
+        <MdFavorite size={60} color="#E7308A" />
+      </Favorite>
+    </Container>
+  );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,39 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import { Link } from 'react-router-dom';
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+  img {
+    width: 300px;
+  }
+`;
+
+export const Favorite = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #000;
+    }
+
+    span {
+      font-size: 12px;
+      color: #010101;
+    }
+  }
+`;
