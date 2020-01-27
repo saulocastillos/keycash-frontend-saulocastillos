0dccbdbe6200a5195114a6575d665cf2aca6df3c
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-27 11:36:20 -0300

    add property page

--- src/pages/Property/index.js
+++ src/pages/Property/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
-export default function Property({ match }) {
+function Property({ match, properties = [] }) {
+  const { id } = match.params;
+  const item = properties.filter(item => item.id === id);
+  let property = null;
+
+  try {
+    property = item[0];
+    console.log(property.address);
+  } catch (e) {
+    console.log(e);
+  }
+
   return (
-    <>
-      <h1>{match.params.id}</h1>
-    </>
+    <Container>
+      {' '}
+      {property ? (
+        <>
+          <img src={property.images[0]} alt={`${property.images[0]}`} />
+          <h1>R$ {property.price}</h1>
+          <h2>Bathrooms: {property.bathrooms}</h2>
+          <h2>Bedrooms: {property.bedrooms}</h2>
+          <h2>Parking Spaces: {property.parkingSpaces}</h2>
+          <h2>Usable Area: {property.parkingSpaces}</h2>
+        </>
+      ) : (
+        ''
+      )}
+    </Container>
   );
 }
+
+export default connect(state => ({
+  properties: state.properties,
+}))(Property);
--- src/pages/Property/styles.js
+++ src/pages/Property/styles.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components';
 
+import { Link } from 'react-router-dom';
+
+import media from 'styled-media-query';
+
 export const Container = styled.div`
-  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  list-style: none;
+  margin: 10px auto;
+
+  img {
+    max-width: 320px;
+  }
+
+  ${media.greaterThan('small')`
+    margin: 0 auto 20px;
+    img {
+      max-width: 1024px;
+      flex-grow: stretch;
+    }
+  `}
+`;
+
+export const CardLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
 `;
