ea2f14d5d2081019a8019923f6cd431332c5c4d2
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-27 11:34:32 -0300

    add favorites reducer

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 
 import favorites from './favorites/reducer';
+import properties from './properties/reducer';
 
-export default combineReducers({ favorites });
+export default combineReducers({ favorites, properties });
