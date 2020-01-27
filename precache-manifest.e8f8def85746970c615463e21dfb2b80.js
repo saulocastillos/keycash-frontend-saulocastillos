794f02f49149c9feee432ed6a92d4d663dcd7084
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-27 12:34:34 -0300

    Updates

---
+++ precache-manifest.e8f8def85746970c615463e21dfb2b80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f08107af17771bbd8005116ff9d4c69",
+    "url": "/keycash-frontend-saulocastillos/index.html"
+  },
+  {
+    "revision": "0d5ad03b8cc4c25806e7",
+    "url": "/keycash-frontend-saulocastillos/static/js/2.a54f743e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/keycash-frontend-saulocastillos/static/js/2.a54f743e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "847a44865fee7e960c8c",
+    "url": "/keycash-frontend-saulocastillos/static/js/main.9ae012ce.chunk.js"
+  },
+  {
+    "revision": "3f8ecb81f53c28760158",
+    "url": "/keycash-frontend-saulocastillos/static/js/runtime-main.3b9f9130.js"
+  },
+  {
+    "revision": "bd6a1e07ee5986544f3cf9e374715668",
+    "url": "/keycash-frontend-saulocastillos/static/media/logo.bd6a1e07.svg"
+  }
+]);
