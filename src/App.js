28f3c502ddde82a76d8bb4e40e913624ac7fd7fe
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-27 01:07:33 -0300

    set provider

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-
 import { HashRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
 
 import Header from './components/Header';
 import Routes from './routes';
-
 import GlobalStyle from './styles/global';
+
+import store from './store';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <HashRouter>
         <Header />
         <Routes />
         <GlobalStyle />
       </HashRouter>
-    </>
+    </Provider>
   );
 }
 
