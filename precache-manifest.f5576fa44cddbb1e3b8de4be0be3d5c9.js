34784772ddc9c41bfa7b676b20e2da9d259918a1
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-27 12:47:51 -0300

    Updates

---
+++ precache-manifest.f5576fa44cddbb1e3b8de4be0be3d5c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f556dcbe7fc5e730524ac69e85026f0e",
+    "url": "//saulocastillos.github.io/keycash-frontend-saulocastillos/index.html"
+  },
+  {
+    "revision": "f0496754a0b0ebe6ef6c",
+    "url": "//saulocastillos.github.io/keycash-frontend-saulocastillos/static/js/2.8f395309.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "//saulocastillos.github.io/keycash-frontend-saulocastillos/static/js/2.8f395309.chunk.js.LICENSE"
+  },
+  {
+    "revision": "847a44865fee7e960c8c",
+    "url": "//saulocastillos.github.io/keycash-frontend-saulocastillos/static/js/main.9ae012ce.chunk.js"
+  },
+  {
+    "revision": "14c5bab6ef8850fe53e2",
+    "url": "//saulocastillos.github.io/keycash-frontend-saulocastillos/static/js/runtime-main.8ad24be7.js"
+  },
+  {
+    "revision": "bd6a1e07ee5986544f3cf9e374715668",
+    "url": "//saulocastillos.github.io/keycash-frontend-saulocastillos/static/media/logo.bd6a1e07.svg"
+  }
+]);
