65cb3dfaed49309b602161b76ef2ec03f537c5d9
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-27 13:02:25 -0300

    http to https

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://5e148887bce1d10014baea80.mockapi.io/keycash/challenge',
+  baseURL: 'https://5e148887bce1d10014baea80.mockapi.io/keycash/challenge',
 });
 
 export default api;
