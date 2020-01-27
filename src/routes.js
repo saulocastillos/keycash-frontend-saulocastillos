e277460f74e03a6f21b6caf61972c3648bdc815b
Saulo Silva Castillos <altfreedom@gmail.com>
2020-01-26 22:54:40 -0300

    add favorites route

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Property from './pages/Property';
+import Favorites from './pages/Favorites';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/property" component={Property} />
+      <Route path="/favorites" component={Favorites} />
     </Switch>
   );
 }
